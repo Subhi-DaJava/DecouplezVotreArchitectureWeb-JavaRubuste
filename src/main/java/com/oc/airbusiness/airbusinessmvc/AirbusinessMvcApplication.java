0aa93bf7c5bc053159ecea528a02acc79f15873f
Subhi YD <subhy809fr@gmail.com>
2022-09-26 00:26:59 +0200

    Premier commit : Initialisation du projet(web, devTools, Thymeleaf), créé index.html

---
+++ src/main/java/com/oc/airbusiness/airbusinessmvc/AirbusinessMvcApplication.java
@@ -0,0 +1,20 @@
+package com.oc.airbusiness.airbusinessmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot DevTools offre, entre autres,
+ * la possibilité de déployer à chaud votre application (vos modifications seront buildées et déployées automatiquement!).
+ * https://github.com/OpenClassrooms-Student-Center/7137741-decouplez-votre-architecture-web-pour-des-app-java-robustes/tree/part1-chap1
+
+ */
+
+@SpringBootApplication
+public class AirbusinessMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbusinessMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oc/airbusiness/airbusinessmvc/AirbusinessMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oc.airbusiness.airbusinessmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbusinessMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
